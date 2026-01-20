51c6e14cd8eb7ccfd1b44e333be86e30e8252edb
나원지 <rowankimna@gmail.com>
2026-01-20 06:00:42 +0000

    Fix: Fix error

--- src/App.vue
+++ src/App.vue
@@ -21,12 +21,3 @@ import IntroSection from "@/components/wedding/IntroSection.vue";
   <MealSection />
   <ContributionSection />
 </template>
-
-// <script setup lang="ts">
-// import { onMounted } from "vue";
-
-// onMounted(() => {
-//   window.scrollTo(0, 0);
-// });
-// </script>
-
